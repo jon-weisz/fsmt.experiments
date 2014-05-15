86aaeea40281f04e27f59442e45fa8bdddd030fd
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-05-15 11:47:46 +0200

    Make png and window working

--- oncilla-sim-0.2/tools/onc-sim0.2-plot-sine.sh
+++ oncilla-sim-0.2/tools/onc-sim0.2-plot-sine.sh
@@ -6,6 +6,12 @@ set xlabel "Time Step"
 set ylabel "X Position in Metres"
 set term png
 set output "/tmp/oncilla-sine-result.png"
+plot name using 1:2 with linespoints notitle
+
+name="/tmp/oncilla-result.csv"
+set title "Oncilla-Sim-0.2-Sine"
+set xlabel "Time Step"
+set ylabel "X Position in Metres"
 set term x11
 set out
 plot name using 1:2 with linespoints notitle
