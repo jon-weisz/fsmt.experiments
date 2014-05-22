78d6fdb5eb717715fde6464af2877eefbbdd10c8
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-05-22 14:25:59 +0200

    path of gnuplot

--- morse-find-human-1.2/tools/hri-gplot.sh
+++ morse-find-human-1.2/tools/hri-gplot.sh
@@ -7,6 +7,6 @@ set ylabel "Position in meters"
 set datafile sep ','
 set style line 1 linewidth 3
 set term png
-set output '$WORKSPACE/$FSMTRA.png'
+set output '/tmp/$USER/fsmt/$FSMTRA/$FSMTRA.png'
 plot '$1' u 4:1 w lines title '$3'
 __EOF
