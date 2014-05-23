7d96f7bfc2889b3bd135e0005d33447bbb22cfc5
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-05-23 22:26:04 +0200

    Short runtime and final position check

--- morse-find-human-1.2/tools/hri-check-position.py
+++ morse-find-human-1.2/tools/hri-check-position.py
@@ -20,15 +20,15 @@ class Checker:
                         assert float(first) == -1.00
                     except AssertionError, e:
                         print "Error, position is not -1.00 but %s" % first
-                        time.sleep(10)
+                        time.sleep(5)
                         sys.exit(1)
                 if x == leng:
                     last = line[0:5]
                     try:
-                        assert float(last) == 0.711
+                        assert float(last) == 0.158
                     except AssertionError, e:
                         print "Error, last is not 0.711 but %s" % last
-                        time.sleep(10)
+                        time.sleep(5)
                         sys.exit(1)
 
 
