63090ebfb8b034c8c73fd5069ad150da449ece8f
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-05-23 22:56:10 +0200

    testing

--- morse-find-human-1.2/tools/hri-start-robot-ros.py
+++ morse-find-human-1.2/tools/hri-start-robot-ros.py
@@ -2,7 +2,7 @@ import subprocess
 
 p = subprocess.Popen(['rostopic', 'pub', '-1', '/robot/motion_vw', 'geometry_msgs/Twist', "{linear: {x: 0.0}, angular: {z: 0.6}}"])
 p.wait()
-p_0 = subprocess.Popen(['rostopic', 'pub', '-1', '/robot/motion_vw', 'geometry_msgs/Twist', "{linear: {x: 0.2}, angular: {z: 0.0}}"])
+p_0 = subprocess.Popen(['rostopic', 'pub', '-1', '/robot/motion_vw', 'geometry_msgs/Twist', "{linear: {x: 0.2}, angular: {z: 0.1}}"])
 p_0.wait()
 p_1 = subprocess.Popen(['rostopic', 'pub', '-1', '/robot/motion_vw', 'geometry_msgs/Twist', "{linear: {x: 0.0}, angular: {z: 0.0}}"])
 p_1.wait()
