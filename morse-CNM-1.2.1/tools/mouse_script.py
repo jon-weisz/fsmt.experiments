374ba7563852a34cf6c8312f840444d55f82ed35
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-10-15 14:49:37 +0200

    motion vm for CNM

---
+++ morse-CNM-1.2.1/tools/mouse_script.py
@@ -0,0 +1,20 @@
+""" Simple script for the CAT AND MOUSE game tutorial
+
+This will command the CAT, using two semantic cameras, to follow
+after the MOUSE robot """
+
+import time
+from pymorse import Morse
+
+def main():
+    """ Use the semantic cameras to locate the target and follow it """
+    with Morse() as morse:
+        motion = morse.mouse.motion
+
+        while True:
+            v_w = {"v": 1, "w": 0.3}
+            time.sleep(0.5)
+            motion.publish(v_w)
+
+if __name__ == "__main__":
+    main()
