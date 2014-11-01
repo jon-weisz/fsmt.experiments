0c9a0d1f5e532b67d88b86ac5f84f671f9d1d86a
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-01 17:40:30 +0100

    Angle polling script

---
+++ icub-nightly/tools/icub-nightly-jointpos.py
@@ -0,0 +1,42 @@
+__author__ = 'flier'
+
+# Example taken from :
+# http://wiki.icub.org/iCub_documentation/python-motor-control_8py_source.html
+# I just made the syntax a little more "pythonic" and removed ";"
+
+import yarp
+import time
+
+yarp.Network.init()
+
+# Prepare a property object
+props = yarp.Property()
+props.put("device", "remote_controlboard")
+props.put("local", "/client/right_arm")
+props.put("remote", "/icubSim/right_arm")
+
+# Create remote driver
+arm_driver = yarp.PolyDriver(props)
+
+# Query motor control interfaces
+i_pos = arm_driver.viewIPositionControl()
+i_vel = arm_driver.viewIVelocityControl()
+i_enc = arm_driver.viewIEncoders()
+
+# Retrieve number of joints
+jnts = i_pos.getAxes()
+
+# Read encoders
+encoders = yarp.Vector(jnts)
+
+# Print Joint Values
+while True:
+    i_enc.getEncoders(encoders.data())
+    tmp = yarp.Vector(jnts)
+
+    print tmp.get(0)
+    print tmp.get(1)
+    print tmp.get(2)
+    print tmp.get(3)
+
+    time.sleep(0.3)
