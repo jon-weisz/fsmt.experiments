9ca9d719b752e381c314e877a29ee2da2c4cfb56
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 17:34:25 +0100

    added prefix for yarp stuff

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -4,16 +4,16 @@ echo "== Connecting YARP ports =="
 
 sleep 1
 
-yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
-yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
-yarp connect /icub/torso/state:o /iCubGui/torso:i
-yarp connect /icub/head/state:o /iCubGui/head:i
-yarp connect /icub/camcalib/left/out /leftCam
-yarp connect /icub/camcalib/right/out /rightCam
-yarp read /icub-nightly/right_arm_dumper | tee $FSMBASE/logs/right_arm.dump &
-yarp read /icub-nightly/left_arm_dumper | tee $FSMBASE/logs/left_arm.dump &
-yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper
-yarp connect /icub/left_arm/state:o /icub-nightly/left_arm_dumper
+$prefix/bin/yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
+$prefix/bin/yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
+$prefix/bin/yarp connect /icub/torso/state:o /iCubGui/torso:i
+$prefix/bin/yarp connect /icub/head/state:o /iCubGui/head:i
+$prefix/bin/yarp connect /icub/camcalib/left/out /leftCam
+$prefix/bin/yarp connect /icub/camcalib/right/out /rightCam
+$prefix/bin/yarp read /icub-nightly/right_arm_dumper | tee $FSMBASE/logs/right_arm.dump &
+$prefix/bin/yarp read /icub-nightly/left_arm_dumper | tee $FSMBASE/logs/left_arm.dump &
+$prefix/bin/yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper
+$prefix/bin/yarp connect /icub/left_arm/state:o /icub-nightly/left_arm_dumper
 
 echo "== YARP Connected =="
 
