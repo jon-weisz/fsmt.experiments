9ca19cf6e01a2ed14d6bc00a653e4bad44c86f5b
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 15:19:16 +0100

    tee in file

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -4,14 +4,14 @@ echo "== Connecting yarp ports =="
 
 sleep 2
 
-yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
-yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
-yarp connect /icub/torso/state:o /iCubGui/torso:i
-yarp connect /icub/head/state:o /iCubGui/head:i
-yarp connect /icub/camcalib/left/out /leftCam
-yarp connect /icub/camcalib/right/out /rightCam
-yarp read /icub-nightly/right_arm_dumper  | tee $FSMBASE/logs/right_arm.dump
-yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper
+yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i &
+yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i &
+yarp connect /icub/torso/state:o /iCubGui/torso:i &
+yarp connect /icub/head/state:o /iCubGui/head:i &
+yarp connect /icub/camcalib/left/out /leftCam &
+yarp connect /icub/camcalib/right/out /rightCam &
+yarp read /icub-nightly/right_arm_dumper  | tee $FSMBASE/logs/right_arm.dump &
+yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper &
 
 echo "== Connected =="
 
