d869a5dbeddd744fe887843544c76c2486733f95
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 12:36:56 +0100

    Forgot to add the datasetplayer to start order

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -6,13 +6,14 @@ yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
 yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
 yarp connect /icub/torso/state:o /iCubGui/torso:i
 yarp connect /icub/head/state:o /iCubGui/head:i
-yarp connect /icub/left_arm/state:o /iCubGui/left_leg:i
+yarp connect /icub/camcalib/left/out /leftCam
+yarp connect /icub/camcalib/right/out /rightCam
 
 echo "== Connected =="
 
 # Not used, yet.
-# /icub/camcalib/left/out
-# /icub/camcalib/right/out
+#
+#
 # /icub/inertial
 # yarp connect /icub/right_leg/state:o /iCubGui/right_leg:i
 # yarp connect /icub/left_leg/state:o /iCubGui/left_leg:i
