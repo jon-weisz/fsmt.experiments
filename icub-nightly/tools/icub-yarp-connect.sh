cb32bc75edbe0736f8752be76105fec877983637
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 15:26:21 +0100

    tee in file for left and right arm

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -7,14 +7,14 @@ sleep 2
 yarp read /icub-nightly/right_arm_dumper | tee $FSMBASE/logs/right_arm.dump &
 yarp read /icub-nightly/left_arm_dumper | tee $FSMBASE/logs/left_arm.dump &
 
-yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
-yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
-yarp connect /icub/torso/state:o /iCubGui/torso:i
-yarp connect /icub/head/state:o /iCubGui/head:i
-yarp connect /icub/camcalib/left/out /leftCam
-yarp connect /icub/camcalib/right/out /rightCam
-yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper
-yarp connect /icub/left_arm/state:o /icub-nightly/left_arm_dumper
+yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i &
+yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i &
+yarp connect /icub/torso/state:o /iCubGui/torso:i &
+yarp connect /icub/head/state:o /iCubGui/head:i &
+yarp connect /icub/camcalib/left/out /leftCam &
+yarp connect /icub/camcalib/right/out /rightCam &
+yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper &
+yarp connect /icub/left_arm/state:o /icub-nightly/left_arm_dumper &
 
 echo "== Connected =="
 
