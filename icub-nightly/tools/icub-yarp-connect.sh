cd3fecb211c5d8f606f36f4c42326519947e09ef
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 19:16:10 +0100

    ctrlz

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -2,16 +2,12 @@
 
 echo "== Connecting YARP ports =="
 
-sleep 1
-
 `yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i`
 `yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i`
 `yarp connect /icub/torso/state:o /iCubGui/torso:i`
 `yarp connect /icub/head/state:o /iCubGui/head:i`
 `yarp connect /icub/camcalib/left/out /leftCam`
 `yarp connect /icub/camcalib/right/out /rightCam`
-
-sleep 1
 
 `yarp read /icub-nightly/right_arm_dumper | tee $FSMBASE/logs/right_arm.dump` &
 `yarp read /icub-nightly/left_arm_dumper | tee $FSMBASE/logs/left_arm.dump` &
@@ -24,9 +20,3 @@ while true; do
     echo "Running..."
     sleep 3
 done
-
-
-# Not used, yet.
-# /icub/inertial
-# yarp connect /icub/right_leg/state:o /iCubGui/right_leg:i
-# yarp connect /icub/left_leg/state:o /iCubGui/left_leg:i
