ad169ca8cbb03936ebdd9a7694ccd622b36f1cff
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 17:58:25 +0100

    Fine tuning

--- icub-nightly/tools/icub-plot-left-arm.sh
+++ icub-nightly/tools/icub-plot-left-arm.sh
@@ -10,5 +10,5 @@ set key reverse Left outside
 set grid
 set style data linespoints
 set output '/tmp/$USER/fsmt/$FSMTRA/icub-right-arm.png'
-plot '$1' using :1 with linespoints title "left_arm[0]"
+plot '$1' using :2 with linespoints title "left_arm[0]"
 __EOF
