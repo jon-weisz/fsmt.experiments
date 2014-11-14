2ecb1013fbe21c3a75a228049da1043667781774
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-14 12:50:13 +0100

    longer sleep for connect

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -2,6 +2,8 @@
 
 echo "== Connecting yarp ports =="
 
+sleep 2
+
 yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
 yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
 yarp connect /icub/torso/state:o /iCubGui/torso:i
@@ -10,8 +12,6 @@ yarp connect /icub/camcalib/left/out /leftCam
 yarp connect /icub/camcalib/right/out /rightCam
 
 echo "== Connected =="
-
-sleep 10
 
 # Not used, yet.
 # /icub/inertial
