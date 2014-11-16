4e2f74b2d836744bafc97a84332e31246134b6c0
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-16 11:13:07 +0100

    keys for plot

--- icub-nightly/tools/icub-plot-left-arm.sh
+++ icub-nightly/tools/icub-plot-left-arm.sh
@@ -6,9 +6,10 @@ set terminal png
 set xlabel "step"
 set ylabel "joint value"
 set title "iCub-Nightly Balltracking left_arm"
-set key reverse Left outside
+# set key reverse Left outside
+set key at screen 0.5,screen 0.1
 set grid
-set style data linespoints
+set style data points
 set output '/tmp/$USER/fsmt/$FSMTRA/icub-left-arm.png'
 plot '$1' using :2 with linespoints title "left_arm[1]", \
 '$prefix/etc/fsmt-experiments/icub-nightly/reference-data/left_arm.dump' using :2 with points title "reference left_arm[1]"
--- icub-nightly/tools/icub-plot-right-arm.sh
+++ icub-nightly/tools/icub-plot-right-arm.sh
@@ -3,12 +3,13 @@
 /usr/bin/gnuplot <<__EOF
 reset
 set terminal png
-set xlabel "step"
-set ylabel "joint value"
+set xlabel "step [yarp]"
+set ylabel "joint value [degree]"
 set title "iCub-Nightly Balltracking right_arm"
-set key reverse Left outside
+# set key reverse Left outside
+set key at screen 0.5,screen 0.1
 set grid
-set style data linespoints
+set style data points
 set output '/tmp/$USER/fsmt/$FSMTRA/icub-right-arm.png'
 plot '$1' using :2 with lines title "right_arm[1]", \
 '$prefix/etc/fsmt-experiments/icub-nightly/reference-data/right_arm.dump' using :2 with points title "reference right_arm[1]"
