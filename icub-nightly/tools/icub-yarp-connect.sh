ac6d2dd52fd06a9e4a478896ac1dcf6658dd4b69
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-16 14:08:29 +0100

    Removed >2&1

--- icub-nightly/tools/icub-yarp-connect.sh
+++ icub-nightly/tools/icub-yarp-connect.sh
@@ -2,16 +2,16 @@
 
 echo "== Connecting YARP ports =="
 
-sleep 1
+yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i
+yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i
+yarp connect /icub/torso/state:o /iCubGui/torso:i
+yarp connect /icub/head/state:o /iCubGui/head:i
+yarp connect /icub/camcalib/left/out /leftCam
+yarp connect /icub/camcalib/right/out /rightCam
+yarp connect /icub/left_arm/state:o /icub-nightly/left_arm_dumper > /dev/null 2>&1
+yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper > /dev/null 2>&1
 
-`yarp connect /icub/left_arm/state:o /iCubGui/left_arm:i`
-`yarp connect /icub/right_arm/state:o /iCubGui/right_arm:i`
-`yarp connect /icub/torso/state:o /iCubGui/torso:i`
-`yarp connect /icub/head/state:o /iCubGui/head:i`
-`yarp connect /icub/camcalib/left/out /leftCam`
-`yarp connect /icub/camcalib/right/out /rightCam`
-`yarp connect /icub/left_arm/state:o /icub-nightly/left_arm_dumper > /dev/null 2>&1`
-`yarp connect /icub/right_arm/state:o /icub-nightly/right_arm_dumper > /dev/null 2>&1`
+sleep 1
 
 echo "== YARP Connected =="
 
