4960947f224022f613b72535790f407ab1bfadf6
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-16 11:06:10 +0100

    Gnuplot fix

--- icub-nightly/tools/icub-plot-left-arm.sh
+++ icub-nightly/tools/icub-plot-left-arm.sh
@@ -10,6 +10,6 @@ set key reverse Left outside
 set grid
 set style data linespoints
 set output '/tmp/$USER/fsmt/$FSMTRA/icub-left-arm.png'
-plot '$1' using :2 with linespoints title "left_arm[1]"
-plot $prefix/etc/fsmt-experiments/icub-nightly/reference-data/left_arm.dump using :2 with linespoints title "reference left_arm[1]"
+plot '$1' using :2 with linespoints title "left_arm[1]", \
+'$prefix/etc/fsmt-experiments/icub-nightly/reference-data/left_arm.dump' using :2 with linespoints title "reference left_arm[1]"
 __EOF
--- icub-nightly/tools/icub-plot-right-arm.sh
+++ icub-nightly/tools/icub-plot-right-arm.sh
@@ -10,6 +10,6 @@ set key reverse Left outside
 set grid
 set style data linespoints
 set output '/tmp/$USER/fsmt/$FSMTRA/icub-right-arm.png'
-plot '$1' using :2 with linespoints title "right_arm[1]"
-plot $prefix/etc/fsmt-experiments/icub-nightly/reference-data/right_arm.dump using :2 with linespoints title "reference right_arm[1]"
+plot '$1' using :2 with linespoints title "right_arm[1]", \
+'$prefix/etc/fsmt-experiments/icub-nightly/reference-data/right_arm.dump' using :2 with linespoints title "reference right_arm[1]"
 __EOF
