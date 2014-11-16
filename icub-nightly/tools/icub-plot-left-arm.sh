803457aa225a8997847d1f9cf93b835e8ca04520
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-16 10:59:34 +0100

    Added reference data and two plots

--- icub-nightly/tools/icub-plot-left-arm.sh
+++ icub-nightly/tools/icub-plot-left-arm.sh
@@ -11,4 +11,5 @@ set grid
 set style data linespoints
 set output '/tmp/$USER/fsmt/$FSMTRA/icub-left-arm.png'
 plot '$1' using :2 with linespoints title "left_arm[1]"
+plot $prefix/etc/fsmt-experiments/icub-nightly/reference-data/left_arm.dump using :2 with linespoints title "reference left_arm[1]"
 __EOF
