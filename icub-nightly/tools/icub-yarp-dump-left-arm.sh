5cf4a23f4d46c58ab6b3a393ff3f8b4287f1054f
Florian Lier <flier@techfak.uni-bielefeld.de>
2014-11-16 14:14:56 +0100

    Removed >2&1

--- icub-nightly/tools/icub-yarp-dump-left-arm.sh
+++ icub-nightly/tools/icub-yarp-dump-left-arm.sh
@@ -3,3 +3,7 @@
 echo "== Reading YARP ports =="
 
 yarp read /icub-nightly/left_arm_dumper | tee $FSMBASE/logs/left_arm.dump
+
+while true; do
+    sleep 2
+done
--- icub-nightly/tools/icub-yarp-dump-right-arm.sh
+++ icub-nightly/tools/icub-yarp-dump-right-arm.sh
@@ -3,3 +3,7 @@
 echo "== Reading YARP ports =="
 
 yarp read /icub-nightly/right_arm_dumper | tee $FSMBASE/logs/right_arm.dump
+
+while true; do
+    sleep 2
+done
