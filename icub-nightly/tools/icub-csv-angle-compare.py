817c7cf34712b506361aa31018e06232d2d8db87
Florian Lier <flier@techfak.uni-bielefeld.de>
2015-02-21 19:24:44 +0100

    localhost for yarp server

---
+++ icub-nightly/tools/icub-csv-angle-compare.py
@@ -0,0 +1,42 @@
+__author__ = 'flier'
+
+import os
+import sys
+import csv
+import time
+
+logpath = os.environ['$FSMLOG']
+
+logfile = "/right_arm.dump"
+raw_file = logpath + logfile
+
+csv_repr = "/right_arm.csv"
+csv_file = logpath + csv_repr
+
+# Create an actual CSV File
+with open(raw_file) as infile, open(csv_file, 'w') as outfile:
+    outfile.write(infile.read().replace(" ", ", "))
+
+# Accessible representation
+joint_2 = []
+
+with open(csv_file) as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        joint_2.append(float(row[1]))
+
+last = len(joint_2)
+first_value = joint_2[0]
+last_value = joint_2[last - 1]
+
+time.sleep(2)
+
+if first_value < 25.0 or first_value > 35.0:
+    print "First joint value is off by 5 degree: " % str(first_value)
+    sys.exit(1)
+
+if last_value < 25.0 or last_value > 35.0:
+    print "Last joint value is off by 5 degree" % str(first_value)
+    sys.exit(1)
+
+print "First and last joint values are correct"
