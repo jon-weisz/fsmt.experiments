e43b368755506f1975c9aacca33d47d78ea5e14b
Florian Lier <flier@techfak.uni-bielefeld.de>
2015-02-22 12:09:12 +0100

    Added strands set camera position

--- strands-patrolling-simulation-nightly/tools/strands_set_camera.py
+++ strands-patrolling-simulation-nightly/tools/strands_set_camera.py
@@ -1,7 +1,11 @@
 __author__ = 'flier'
 
+import os
+import sys
 import time
 import pymorse
+
+sys.path.append('/opt/strands-morse-simulator/lib/python3/dist-packages/')
 
 with pymorse.Morse() as sim:
     sim.rpc('simulation', 'set_camarafp_position' [10, 10, 10])
